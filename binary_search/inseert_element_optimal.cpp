bcba528995653060f991312553ca4ed67b21f4ee
SarthakRajvaidya <sarthakrajvaidya2506@gmail.com>
2026-08-30 20:43:03 +0530

    Day 21 : Insert element using binary search

---
+++ binary_search/inseert_element_optimal.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int target;
+    cin>>target;
+
+    vector<int>arr(n);
+
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+
+    }
+
+    int low = 0;
+    int high = n-1;
+    int ans = n;
+
+    while(low<=high){
+        int mid = (low+high)/2;
+
+        if(arr[mid]>=target){
+            ans = mid;
+            high = mid-1;
+
+
+        }
+        else{
+            low = mid+1;
+        }
+    }
+
+    cout<<ans;
+}
