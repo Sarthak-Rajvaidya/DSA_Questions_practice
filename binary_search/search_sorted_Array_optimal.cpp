8d04c0af28b590b9b94338b792e14ca1f4249eb0
SarthakRajvaidya <sarthakrajvaidya2506@gmail.com>
2026-09-04 19:07:43 +0530

    Day 25 : Search in rotated sorted array using Binary Search

---
+++ binary_search/search_sorted_Array_optimal.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int target;
+    cin>>target;
+
+    vector<int>arr(n);
+
+    for(int i =0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    int low = 0;
+    int high = n-1;
+
+    while(low<=high){
+        int mid = (low+high)/2;
+
+        if(arr[mid] == target){
+            cout<<mid<<endl;
+            return 0;
+        }
+
+        if(arr[low]<=arr[mid]){
+            if(arr[low] <= target && target < arr[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            
+            }
+        }
+
+        else{
+            if(arr[mid] <= target && target <= arr[high]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+
+
+
+    }
+
+    cout<<-1<<endl;
+    return 0;
+
+}
